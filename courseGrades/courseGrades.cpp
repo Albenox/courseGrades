506a4f6e3d649cc25d1d82197ae604a16ab4f470
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-18 16:22:23 -0400

    Add project files.

---
+++ courseGrades/courseGrades.cpp
@@ -0,0 +1,28 @@
+/*
+1. Read the header values indicating the number of students and tests.
+2. Dynamically allocate an array of student structs.
+3. Dynamically allocate an array of test scores inside each struct.
+4. Store student name, ID, test scores, average, and letter grade.
+5. Calculate average test scores.
+6. Assign letter grades using the provided grading scale.
+7. Produce a neatly formatted report.
+8. Deallocate memory in the correct order:
+8.1- Test score arrays
+8.2- Student array
+*/
+
+//Libraries and namespace
+#include <iostream>
+using namespace std;
+
+//Struct definition that holds all variables for student
+struct student {
+    string name;
+    int grade;
+};
+
+//Main function
+int main()
+{
+    return 0;
+}
