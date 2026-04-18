b069423bf84beff0578b44d58499503089351002
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-18 18:38:51 -0400

    Made a program that can read the file and print it back out, just to make sure the file can even be read, which then means it can be saved and used for the features of the program

--- courseGrades/courseGrades.cpp
+++ courseGrades/courseGrades.cpp
@@ -12,17 +12,39 @@
 */
 
 //Libraries and namespace
-#include <iostream>
+#include <iostream> 
+#include <stdio.h>
 using namespace std;
-
-//Struct definition that holds all variables for student
-struct student {
-    string name;
-    int grade;
-};
 
 //Main function
 int main()
 {
-    return 0;
+	//Declares a pointer to a file stream and initializes it to nullptr
+    FILE *pF = nullptr;
+	//Open the file for reading
+    fopen_s(&pF, "student_data.txt", "r");
+
+	//Checks if the file was successfully opened, which if not, ending the program with an error message
+    if (pF == NULL) {
+        cout << "Error: file could not be opened." << endl;
+        return 1;
+    }
+    //Allows the program to continue if the file was successfully opened
+
+    else {
+		//Declares a character buffer to hold each line read from the file, with a maximum size of 255 characters
+        char buffer[255];
+
+		//Reads the file line by line and prints each line to the console until the end of the file is reached
+        //This is done by running fgets and checking to see if it returns NULL, which indicates the end of the file if so, ending the loop
+        while (fgets(buffer, 255, pF) != NULL) {
+			//Prints the contents of the buffer, which is the line read from the file, to the console
+            printf("%s", buffer);
+        }
+
+		//Closes the file after reading is complete
+        fclose(pF);
+
+        return 0;
+    }
 }
