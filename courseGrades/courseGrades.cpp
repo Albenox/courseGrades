55d96133608113bdfd7ba18d1d0f15637c82b10e
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-19 19:54:44 -0400

    A bit of an everywhere commit, but it shows that I am testing converting the char buffer into a string to make tokens that can easily break apart the lines inside the txt file ,which would make taking the information so much easier, so the next step is to rearrange the existing code to not rely on the char buffer anymore and instead use a string system.

--- courseGrades/courseGrades.cpp
+++ courseGrades/courseGrades.cpp
@@ -14,37 +14,88 @@
 //Libraries and namespace
 #include <iostream> 
 #include <stdio.h>
+#include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
 
+//string studentsTests(char buffer);
+
+struct student {
+    //Variables for student struct
+    string name;
+    int id;
+	//Pointer to an array of test scores, which will be dynamically allocated later
+    double* testScores;
+    double average;
+	char letterGrade;
+};
+
 //Main function
-int main()
-{
+int main()  {
+
+	int numStudents;
+	int numTests;
+
 	//Declares a pointer to a file stream and initializes it to nullptr
-    FILE *pF = nullptr;
+    FILE *pFile = nullptr;
 	//Open the file for reading
-    fopen_s(&pF, "student_data.txt", "r");
+    fopen_s(&pFile, "student_data.txt", "r");
 
 	//Checks if the file was successfully opened, which if not, ending the program with an error message
-    if (pF == NULL) {
+    if (pFile == NULL) {
         cout << "Error: file could not be opened." << endl;
         return 1;
     }
     //Allows the program to continue if the file was successfully opened
-
     else {
 		//Declares a character buffer to hold each line read from the file, with a maximum size of 255 characters
         char buffer[255];
+        
+
+        fgets(buffer, sizeof(buffer), pFile);
+        string line = buffer;
+        //stringstream ss(line) automaticall creates tokens from the line seperated by spaces, that automatically progresses through the line each time ss is called
+		//This allows us to call ss twice in a row, and since the first line will always be the number of students and tests, both variables are easily populated
+        stringstream ss(line);
+        //First token that is the number of students
+        ss >> numStudents;
+		//Second token that is the number of tests
+        ss >> numTests;
+
+		cout << "Number of students: " << numStudents << endl;
+		cout << "Number of tests: " << numTests << endl;
+
+        //TESTING STRING
+		int TESTnumStudents;
+		int TESTnumTests;
+		ifstream TESTfile("student_data.txt");
+        string TESTline;
+        getline(TESTfile , TESTline);
+        stringstream TESTss(TESTline);
+		TESTss >> TESTnumStudents;
+		TESTss >> TESTnumTests;
+		cout << "Testing number of students: " << TESTnumStudents << endl;
+		cout << "Testing number of tests: " << TESTnumTests << endl;
+        //TESTING STRING
+
 
 		//Reads the file line by line and prints each line to the console until the end of the file is reached
         //This is done by running fgets and checking to see if it returns NULL, which indicates the end of the file if so, ending the loop
-        while (fgets(buffer, 255, pF) != NULL) {
+        while (fgets(buffer, sizeof(buffer), pFile) != NULL) {
 			//Prints the contents of the buffer, which is the line read from the file, to the console
             printf("%s", buffer);
         }
 
 		//Closes the file after reading is complete
-        fclose(pF);
+        fclose(pFile);
 
+		//Returns 0 to indicate that the program ended successfully
         return 0;
     }
 }
+/*
+string studentsTests(char buffer) {
+    cout << "WIP";
+}
+*/
