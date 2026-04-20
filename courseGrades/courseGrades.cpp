a84059185a9e7ef20f44262cc77d37df2474d62c
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-19 21:44:32 -0400

    A near finalized version just missing the delete[] to ensure no memory leaks occur, but otherwise calculates and outputs all information needed.

--- courseGrades/courseGrades.cpp
+++ courseGrades/courseGrades.cpp
@@ -13,13 +13,11 @@
 
 //Libraries and namespace
 #include <iostream> 
+#include <iomanip>
 #include <fstream>
 #include <string>
 #include <sstream>
 using namespace std;
-
-//Function to extract the number of students and tests from the first line of the file, linked to the addresses so the values do not need to be returned, and can be a void function
-void studentsTests(const string &line, int &numStudents, int &numTests);
 
 //A struct for students
 struct student {
@@ -27,10 +25,16 @@ struct student {
     string name;
     int id;
 	//Pointer to an array of test scores, which will be dynamically allocated later
-    double* testScores;
+    int* testScores;
     double average;
 	char letterGrade;
 };
+
+//Function to extract the number of students and tests from the first line of the file, linked to the addresses so the values do not need to be returned, and can be a void function
+void studentsTests(const string& line, int& numStudents, int& numTests);
+
+//Function to print the report of students, their test scores, averages, and letter grades in a formatted manner
+void printReport(const student* students, int numStudents, int numTests);
 
 //Main function
 int main()  {
@@ -49,6 +53,8 @@ int main()  {
         int numStudents, numTests;
         //Declares the string variable "line", which will hold the current line read from getline()
         string line;
+		//Declares double variable to track the total of all test scores for a student, which will be used to calculate the average test score for each student
+		double testTotal = 0;
 		
         //Grabs a line from the file and stores it into the string variable "line", which automatically progresses to the next line for the next time getline is called 
         //Sidenote, getline is perfect and makes this SO MUCH EASIER I LOVE IT!!!
@@ -70,14 +76,43 @@ int main()  {
             ss >> students[i].name;
 			ss >> students[i].id;
 
+			//Dynamically allocates an array of test scores inside each struct, with the size of the array equal to the number of tests declared on the first line
+            students[i].testScores = new int[numTests];
+
+            //Resets the total variable to 0 for the next student
+            testTotal = 0;
+
 			//A for loop that runs sets a number of tokens into the current student's test score array equal to the number of tests declared on the first line
             for (int x = 0; x < numTests; x++) {
                 //Extracts the test score for the current test and stores it in the current student's test score array at the index of the current test
                 ss >> students[i].testScores[x];
+				testTotal += students[i].testScores[x];
+            }
+            
+            //Calculates the average test score for the current student
+            students[i].average = testTotal / numTests;
+
+            //Assigns a letter grade to the current student based on their average test score using the provided grading scale, and stores it in the current student's letter grade variable
+            if (students[i].average >= 90) {
+                students[i].letterGrade = 'A';
+            }
+            else if (students[i].average >= 80) {
+                students[i].letterGrade = 'B';
+            }
+            else if (students[i].average >= 70) {
+                students[i].letterGrade = 'C';
+            }
+            else if (students[i].average >= 60) {
+                students[i].letterGrade = 'D';
+            }
+            else {
+                students[i].letterGrade = 'F';
             }
 		}
-
-		//Returns 0 to indicate that the program ended successfully
+		//Function to print the report of students, their test scores, averages, and letter grades in a formatted manner
+        printReport(students, numStudents, numTests);
+		
+        //Returns 0 to indicate that the program ended successfully
         return 0;
     }
 }
@@ -90,7 +125,38 @@ void studentsTests(const string &line, int &numStudents, int &numTests) {
     //Extracts and automatically runs through tokens of the line each time ss is called
     ss >> numStudents;
     ss >> numTests;
+}
 
-    cout << "Testing number of students: " << numStudents << endl;
-    cout << "Testing number of tests: " << numTests << endl;
+void printReport(const student* students, int numStudents, int numTests) {
+    cout << "\n===== Course Grade Report =====\n\n";
+
+    //Set formatting for decimals
+    cout << fixed << setprecision(2);
+
+    //Header row (aligned columns)
+    cout << left << setw(12) << "Name"
+        << setw(10) << "ID"
+        << setw(20) << "Scores"
+        << setw(10) << "Average"
+        << setw(6) << "Grade" << endl;
+
+    cout << "--------------------------------------------------------------\n";
+
+    for (int i = 0; i < numStudents; i++) {
+        //Name and ID
+        cout << left << setw(12) << students[i].name
+            << setw(10) << students[i].id;
+
+        //Scores (print inline)
+        for (int j = 0; j < numTests; j++) {
+            cout << students[i].testScores[j] << " ";
+        }
+
+        //Adjust spacing after scores and keeps columns aligned
+        cout << setw(20 - (numTests * 3)) << " "; 
+
+        //Average and grade
+        cout << setw(10) << students[i].average
+            << setw(6) << students[i].letterGrade << endl;
+    }
 }
